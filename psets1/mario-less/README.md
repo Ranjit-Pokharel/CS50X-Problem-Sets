2a41d27c3c0f0b4aa9bb8b2d9f0148aa3f510751
Ranjit-Pokharel <rp.ranjitpokharel.pr@gmail.com>
2024-11-01 10:37:26 +0545

    Fri Nov  1 10:37:26 AM +0545 2024 mario-less

---
+++ psets1/mario-less/README.md
@@ -0,0 +1,39 @@
+# mario.c
+Print right-aligned pyramid of bricks. at the end of Super mario Bros world 1-1.
+
+## Table of Contents
+- [About](#about)
+- [Getting Started](#getting-started)
+
+## About
+A program that print pyramid of given non negative and non zero height.
+
+## Getting Started
+### Prerequisites
+To compile and run this program, you’ll need:
+- A C compiler, such as GCC or Clang.
+- [cs50 c library](https://cs50.readthedocs.io/libraries/cs50/c/)
+
+### Installation
+   ```
+   git clone https://github.com/Ranjit-Pokharel/CS50X-Problem-Sets.git
+   cd CS50X-Problem-Sets/psets1/mario-less
+   clang -o mario mario.c -lcs50
+   ./mario
+   Height: <not negativ non zero number>
+   ```
+   output ``right aligned pyramid of height``
+
+### Example
+    ```
+    $ ./mario
+    Height: 8
+           #
+          ##
+         ###
+        ####
+       #####
+      ######
+     #######
+    ########
+    ```
---
+++ psets1/mario-less/mario.c
@@ -0,0 +1,94 @@
+/*
+ * File: mario.c
+ * Description: Print right-aligned pyramid of bricks. at the end of Super mario Bros world 1-1.
+ */
+#include <cs50.h>
+#include <stdio.h>
+
+int get_height(char *prompt);
+void draw_pyramid(int height);
+void row_print(int height, int row_level);
+
+/*
+ * Function: main
+ * --------------
+ * take user input height of pyramid draw the pyramid.
+ * 
+ * return: 0
+ */
+int main(void)
+{
+    int height = get_height("Height: ");
+    draw_pyramid(height);
+    return 0;
+}
+
+/*
+ * Function: draw_pyramid
+ * ----------------------
+ * initilize starting row (first row) from where the pyramid is printed.
+ * 
+ * height: int. height of the pyramid to be print.
+ */
+void draw_pyramid(int height)
+{
+    int starting_row = 1;
+    row_print(height, starting_row);
+}
+
+/*
+ * Function: row_print
+ * -------------------
+ * print each row with a brick as #. recursive function.
+ * 
+ * height: int. height of the pyramid.
+ * row_level: int. The row that is to be printed
+ * 
+ * returns: function is return if the height is less then the low_level as row cannot be grater 
+ *          then the given height.
+ */
+void row_print(int height, int row_level)
+{
+    // height cannot be less then row to be print.
+    if (height < row_level)
+    {
+        return;
+    }
+
+    // to make right aligned pyramid
+    // first space is print. there will be hight - row space.
+    for (int i = 0; i < height - row_level; i++)
+    {
+        printf(" ");
+    }
+    // second the brick # is printed which will increase as the row increase.
+    for (int i = 0; i < row_level; i++)
+    {
+        printf("#");
+    }
+    // to go to next row
+    printf("\n");
+
+    // recursive call to print in next row. row is updated to next row.
+    row_print(height, row_level + 1);
+}
+
+/*
+ * Function: get_height
+ * --------------------
+ * get a valid non negative number. as height cannot be negative height and height not zero.
+ * 
+ * prompt: char*. text to be display to ask for user for height.
+ * 
+ * return: int. gives valid not negative number of height*/
+int get_height(char *prompt)
+{
+    int n;
+    // ask user again if input is negative, not int or is zero
+    do
+    {
+        n = get_int("%s", prompt);
+    } 
+    while (n < 1); 
+    return n;   
+}
